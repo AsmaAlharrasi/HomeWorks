07033d11b754a79c0b7235f5a737835013b23dd4
AsmaAlharrasi <asmaalharrasi18@gmail.com>
2023-11-22 09:19:38 +0400

    updated

---
+++ HW2 week 2/HW 2 week 2 solution/HW 2 week 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace HW_2_week_2
+{
+    class Program
+    {
+
+        static void DisplayMenu ( )
+        {
+
+            string[] coffee = { "Latte", "Cappuccino", "Amreicano" };
+            double[] price = { 3.50, 3.00, 2.50 };
+            string[] size = { "Small", "Medium", "Large" };
+
+            Console.WriteLine($"Menu :\n 1.{coffee[0]} -- {price[0]}\n  2.{ coffee[1]} -- { price[1]}\n 3.{coffee[2]} -- {price[2]}");
+            Console.WriteLine("Please enter the desired Drink from (1-3)");
+
+        }
+
+        static void PlaceOrder (  string size, string suger , string milk )
+        {
+
+            string[] Size = { "Latte", "Cappuccino", "Amreicano" };
+            string[] Suger = { "with suger" , "without suger" };
+            string[] Milk = { "with milk" , "without milk" };
+
+
+
+        }
+
+        static void CalculateCost( string coffee, string size)
+        {
+
+        }
+
+        static void displayOrderSummary( string coffee, string size, bool sugar, bool milk, double total)
+        {
+
+        }
+        static void Main()
+        {
+            DisplayMenu();
+        }
+    }
+}
---
+++ Tasks/leet code solution/leet code/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace leet_code
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Example Usage:
+            int n = 16;
+            bool result = IsPowerOfTwo(n);
+
+            Console.WriteLine($"{n} is a power of two: {result}");
+        }
+
+        static bool IsPowerOfTwo(int n)
+        {
+            if (n <= 0)
+            {
+                return false;
+            }
+
+            // Check if only one bit is set in the binary representation
+            return (n & (n - 1)) == 0;
+        }
+    }
+}
