27b385ca83b1fe949a7be4b2ac8a4fc0c0b79b0c
AsmaAlharrasi <asmaalharrasi18@gmail.com>
2023-11-27 22:29:31 +0400

    HW 1 for week 3 is added

--- Tasks/Task 1 solution/Task 1/obj/Debug/net6.0/Task 1.AssemblyInfo.cs
+++ Tasks/Task 1 solution/Task 1/obj/Debug/net6.0/Task 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52844864ac8948e69242513835196bb4208b33b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Tasks/leet code solution/leet code/obj/Debug/net8.0/leet code.AssemblyInfo.cs
+++ Tasks/leet code solution/leet code/obj/Debug/net8.0/leet code.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leet code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52844864ac8948e69242513835196bb4208b33b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("leet code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leet code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
